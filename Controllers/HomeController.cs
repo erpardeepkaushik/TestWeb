ec15c8ed18c8d1770cdeea6dda12577dc8e27b77
pardeep <pardeep@medinyx.com>
2023-12-23 18:12:10 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestWeb.Controllers
+{
+    public class HomeController : Controller
+    {        
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            Session["PageName"] = "Updated from About Page";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            Session["PageName"] = "Updated from Contact Page";
+
+            return View();
+        }
+    }
+}
---
+++ Global.asax.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace TestWeb
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            SystemWebAdapterConfiguration.AddSystemWebAdapters(this)
+              .AddJsonSessionSerializer(options =>
+              {
+                  // Serialization/deserialization requires each session key to be registered to a type
+                  options.RegisterKey<string>("MachineName");
+                  options.RegisterKey<string>("PageName");
+                  options.RegisterKey<DateTime>("SessionStartTime");
+                  //options.RegisterKey<SessionDemoModel>("DemoItem");
+              })
+              .AddRemoteAppServer(options =>
+              {
+                  options.ApiKey = ConfigurationManager.AppSettings["RemoteAppApiKey"];
+              })
+              .AddSessionServer();
+        }
+
+        protected void Session_Start(Object sender, EventArgs e)
+        {
+            HttpContext.Current.Session["MachineName"] = Environment.MachineName;
+            HttpContext.Current.Session["PageName"] = "WebApp";
+            HttpContext.Current.Session["SessionStartTime"] = DateTime.Now;
+        }
+    }
+}
